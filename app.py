a2d0b465d65f21e60c117a10964a182fcc690ef0
OriginalGoku <2144656@protonmail.com>
2024-11-29 16:00:48 -0500

    reply to user input

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from onboarding_tutorial import OnboardingTutorial
 import ssl as ssl_lib
 import certifi
 import os
+from pprint import pprint
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -37,7 +38,7 @@ def start_onboarding(user_id: str, channel: str, client: WebClient):
 @app.event("assistant_thread_started")
 def handle_assistant_thread_started_events(body, logger):
     logger.info(body)
-    
+
 # ================ Team Join Event =============== #
 # When the user first joins a team, the type of the event will be 'team_join'.
 # Here we'll link the onboarding_message callback to the 'team_join' event.
@@ -114,6 +115,17 @@ def update_pin(event, client):
     # Post the updated message in Slack
     updated_message = client.chat_update(**message)
 
+@app.event("pin_removed")
+def handle_pin_removed_events(body, logger):
+    print(f"pin_removed: {body}")
+    logger.info(body)
+
+@app.command("/echo")
+def repeat_text(ack, say, command):
+    # Acknowledge command request
+    print(f"Command: {command}")
+    ack()
+    say(f"{command['text']}")
 
 # ============== Message Events ============= #
 # When a user sends a DM, the event type will be 'message'.
@@ -126,9 +138,23 @@ def message(event, client):
     channel_id = event.get("channel")
     user_id = event.get("user")
     text = event.get("text")
-
-    if text and text.lower() == "start":
+    ts = event.get("ts")
+    
+    print(f"user_id: {user_id}")
+    print(f"channel_id: {channel_id}")
+    print(f"Message: {text}")
+    print(f"ts: {ts}")
+    if text and "*start simulation*" in text.lower():
+        client.chat_postMessage(channel=channel_id, text=f"Replying to {user_id} from {channel_id} with {text} and Time Stamp: {ts}")
         return start_onboarding(user_id, channel_id, client)
+
+# @app.message("message")
+# def message(message, say):
+#     print(f"message: {message}\n\n")
+#     pprint(f"message = {message}")
+#     # say() sends a message to the channel where the event was triggered
+#     say(f"Hey there <@{message['user']}>!")
+    
 
 if __name__ == "__main__":
     ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
